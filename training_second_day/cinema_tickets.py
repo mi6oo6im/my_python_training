79401d68d5a66ec75ee03c6ef1e29b3c5eff1a84
mi6oo6im <54684159+mi6oo6im@users.noreply.github.com>
2022-03-17 19:10:50 +0000

    Create cinema_tickets.py

---
+++ training_second_day/cinema_tickets.py
@@ -0,0 +1,16 @@
+# Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday,
+# 12 12 14 14 12 16 16
+
+daily_ticket = {
+    "Monday": 12,
+    "Tuesday": 12,
+    "Wednesday": 14,
+    "Thursday": 14,
+    "Friday": 12,
+    "Saturday": 16,
+    "Sunday": 16
+}
+
+week_day = input()
+
+print(daily_ticket[week_day])
