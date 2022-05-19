c2648b9db871327ee82f87e8f0278417f71ccf9b
Mihail Istiliyanov <54684159+mi6oo6im@users.noreply.github.com>
2022-05-19 08:30:14 +0100

    Update cows&bulls.py

--- training_projects/cows&bulls/cows&bulls.py
+++ training_projects/cows&bulls/cows&bulls.py
@@ -4,6 +4,8 @@ num_list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]
 print(num_list)
 random.shuffle(num_list)
 print(num_list)
-x = [num_list.pop(0), num_list.pop(0), num_list.pop(0), num_list.pop(0)]
+x = []
+for _ in range(4):
+    x.append(num_list.pop(0))
 print(x)
 print(num_list)
