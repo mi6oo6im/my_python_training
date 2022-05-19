31332d5923bb55dd0d8512488dfc79d114a38a17
Mihail Istiliyanov <54684159+mi6oo6im@users.noreply.github.com>
2022-05-19 19:16:10 +0100

    Update bulls_and_cows.py

--- training_projects/cows&bulls/bulls_and_cows.py
+++ training_projects/cows&bulls/bulls_and_cows.py
@@ -1,8 +1,11 @@
 import random
+
 # the rules
-print("\nBulls and Cows is a logic game for guessing numbers. It is played by two opponents, each trying to guess the secret 4-digit number (without repeating digit) invented by the other.\nAfter each move, the opponent gives the number of matches.")
+print(
+    "\nBulls and Cows is a logic game for guessing numbers. It is played by two opponents, each trying to guess the secret 4-digit number (without repeating digit) invented by the other.\nAfter each move, the opponent gives the number of matches.")
 print("The game is played until you guess correctly the opponent's number or until you type 'Uncle!' in the console.")
-print("If a digit of the guess is contained in the secret number and is in the right place, it is 'bull', if it is in a different place, it is 'cow'.\n")
+print(
+    "If a digit of the guess is contained in the secret number and is in the right place, it is 'bull', if it is in a different place, it is 'cow'.\n")
 # computer's generated 4-digit number
 num_list = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0']
 random.shuffle(num_list)
@@ -23,8 +26,9 @@ while guess != 'Uncle!':
     new_guess_list = list(guess)
     for _ in range(4):
         check = new_guess_list.pop(0)
-        if check in new_guess_list or guess[0] == '0' or len(guess) != 4:
-            print(f'The number you have entered {guess} has repeating digits,starts with 0 or is not 4-digit number, please try again!\n')
+        if check in new_guess_list or guess[0] == '0' or len(guess) != 4 or guess.isdigit() != True:
+            print(
+                f'The number you have entered {guess} has repeating digits,starts with 0 or is not a 4-digit number, please try again!\n')
             guess = input('Make your guess or cry "Uncle!": ')
             number_is_wrong = True
             break
