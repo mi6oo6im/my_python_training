8caaac922ad5bc20c9542e437c3afa4bda9e8f77
Mihail Istiliyanov <54684159+mi6oo6im@users.noreply.github.com>
2022-05-19 10:17:27 +0100

    Update cows&bulls.py
    Added the check for numbers and guess list. Next to add while loop for attempts until we cry "Uncle!"

--- training_projects/cows&bulls/cows&bulls.py
+++ training_projects/cows&bulls/cows&bulls.py
@@ -1,15 +1,27 @@
 import random
+
+# computer's generated 4-digit number
 num_list = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0']
 random.shuffle(num_list)
 number_to_guess = []
 for _ in range(4):
     number_to_guess.append(num_list.pop(0))
-print(number_to_guess)
-print(num_list)
 number_to_guess_string = ''.join(number_to_guess)
 print(number_to_guess_string)
+cows = 0;
+bulls = 0;
+# input
+guess_list =[]
 guess = input('Make your guess: ')
+guess_list.append(guess)
 if guess == number_to_guess_string:
     print('You win!')
 else:
+    for i, j in enumerate(guess):
+        if j == number_to_guess[i]:
+            bulls += 1
+        elif j in number_to_guess:
+            cows += 1
     print('Try again!')
+    print(f'cows: {cows}; bulls: {bulls}')
+    print(f'your guesses so far: {guess_list}')
