0b38889b893c282ae989ee40e9aa877eb5d11d64
Mihail Istiliyanov <54684159+mi6oo6im@users.noreply.github.com>
2022-05-19 08:45:37 +0100

    Update cows&bulls.py

--- training_projects/cows&bulls/cows&bulls.py
+++ training_projects/cows&bulls/cows&bulls.py
@@ -1,11 +1,15 @@
 import random
-
-num_list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]
-print(num_list)
+num_list = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0']
 random.shuffle(num_list)
-print(num_list)
-x = []
+number_to_guess = []
 for _ in range(4):
-    x.append(num_list.pop(0))
-print(x)
+    number_to_guess.append(num_list.pop(0))
+print(number_to_guess)
 print(num_list)
+number_to_guess_string = ''.join(number_to_guess)
+print(number_to_guess_string)
+guess = input('Make your guess: ')
+if guess == number_to_guess_string:
+    print('You win!')
+else:
+    print('Try again!')
