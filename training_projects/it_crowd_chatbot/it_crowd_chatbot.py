865e5151c2ef6f1c0814ff415664546591c096b5
Mihail Istiliyanov <54684159+mi6oo6im@users.noreply.github.com>
2022-05-29 15:54:34 +0100

    bug fix

--- training_projects/it_crowd_chatbot/it_crowd_chatbot.py
+++ training_projects/it_crowd_chatbot/it_crowd_chatbot.py
@@ -2,8 +2,9 @@ from nltk.chat.util import Chat, reflections
 
 pairs = [
     [
-        r"(.*)(my name is|I'm|I am) (.*)",
-        ["Hello %3, IT chatbot here! How can I assist you?", "Hello IT, what is your emergency %3?",
+        r"(.*)(my name is|I'm|I am|this is|im'|i'm) (.*)",
+        ["Hello %3, IT chatbot here! How can I assist you?",
+         "Hello IT, what is your emergency, %3?",
          "Hi there %3, this is IT chatbot, what is on your mind?",
          "Hey, %3, IT chatbot here! What can't wait for a ticket?",
          "Hello %3, this is your IT chatbot, how may I be of service?",
@@ -26,14 +27,14 @@ pairs = [
     [
         r"(.*)(yes|Yes|YES)(.*)",
         ["Did you try turning it off and on again?", "Put it back as it was! Is it working?",
-         "Are there any flashing (green|yellow|orange) lights?", "Turn it on! Is it working?",
+         "Are there any flashing green lights?", "Turn it on! Is it working?",
          "Reinstall the drivers. Is it working?",
          "Did you try jiggling the cables! Did it work?",
          "Try clearing your browser cookies and cache. Is it working?",
          "Try turning it off and on again! Did it work?"]
     ],
     [
-        r"(.*) (works|worked|working|ok|fine|OK|Ok|properly)(.*)",
+        r"(.*)(works|worked|working|ok|fine|OK|Ok|properly|all good)(.*)",
         ["I'm glad this is resolved, please type 'quit' and leave us your feedback!",
          "Great to hear! Have a swell day, mate! Please type 'quit' and leave us your feedback!",
          "You're welcome mate! Please type 'quit' and leave us your feedback!",
@@ -49,7 +50,7 @@ pairs = [
         ["Bye for now. See you soon :)", "It was nice talking to you. See you soon :)",
          "One is glad to be of service! Have a great day ahead :)",
          "It was nice chatting with you. May the odds be ever in your favour! :)",
-         "I enjoyed our chat! May the force be with you Obi! :)"]
+         "I enjoyed our chat! May the force be with you Obi-Wan Kenobi! :)"]
     ],
     [
         r"(.*)",
@@ -60,7 +61,7 @@ pairs = [
 
 # default message at the start of chat
 print("\nWelcome to our new automated IT support powered by NLTK an open source Natural Language Processing toolkit. ",
-      "\n\nPlease start by typing 'Hello, my name is...'\n\nType 'quit' to leave this chat.\n")
+      "\n\nPlease use lower case and start by typing 'Hello, my name is...'\n\nType 'quit' to leave this chat.\n")
 
 # Create Chat Bot
 chat = Chat(pairs, reflections)
