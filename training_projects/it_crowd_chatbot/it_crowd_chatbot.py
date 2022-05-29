bc8e16459afe0524d903cbf662ac0852ff7314eb
Mihail Istiliyanov <54684159+mi6oo6im@users.noreply.github.com>
2022-05-29 12:53:20 +0100

    bug fixes and dialogue enhancement

--- training_projects/it_crowd_chatbot/it_crowd_chatbot.py
+++ training_projects/it_crowd_chatbot/it_crowd_chatbot.py
@@ -10,7 +10,8 @@ pairs = [
          ]
     ],
     [
-        r"(.*)(keyboard|mouse|laptop|monitor|printer|phone|tablet) (.*) (not working|broken|stopped|crashed)(.*)",
+        r"(.*)(computer|browser|program|keyboard|mouse|laptop|monitor|printer|phone|tablet) "
+        r"(.*) (not working|broken|stopped|crashed|down)(.*)",
         ["Did you try turning it off and on again?", "Did you change anything recently?",
          "Are there any flashing lights?", "Are you sure it's turned on?",
          "Did you try jiggling the cables! Did it work?",
@@ -20,33 +21,45 @@ pairs = [
         r"(.*)(no|No|NO)(.*)",
         ["Try jiggling the cables! Did it work?", "Try clearing your browser cookies and history."
          "Try turning it off and on again! Did it work?", "Are you sure it's turned on?",
-         "Put it back as it was! Is it working?", ]
+         "Put it back as it was! Is it working?", "Are there any flashing red lights?" ]
     ],
     [
         r"(.*)(yes|Yes|YES)(.*)",
         ["Did you try turning it off and on again?", "Put it back as it was! Is it working?",
-         "Are there any flashing green lights?", "Turn it on! Is it working?",
+         "Are there any flashing (green|yellow|orange) lights?", "Turn it on! Is it working?",
+         "Reinstall the drivers. Is it working?",
          "Did you try jiggling the cables! Did it work?",
          "Try clearing your browser cookies and cache. Is it working?",
          "Try turning it off and on again! Did it work?"]
     ],
-
+    [
+        r"(.*)(works|working|ok|fine|OK|Ok|properly|)(.*)",
+        ["I'm glad this is resolved, %3, please type 'quit' and leave us your feedback!",
+         "Great to hear, %3! Have a swell day, mate! Please type 'quit' and leave us your feedback!",
+         "You're welcome mate! Please type 'quit' and leave us your feedback!",
+         "See how easy it was, %3, next time you can do it yourself!Please type 'quit' and leave us your feedback!"]
+    ],
+    [
+        r"(.*) (question|query|ask|check|browse|) (.*)",
+        ["I'm sorry, your %2 is out of the scope of IT chatbot, please forward your %2 to our customer support:\n "
+         "email: support@itcrowd.co.uk"]
+    ],
     [
         r"quit",
-        ["Bye for now. See you soon :) ", "It was nice talking to you. See you soon :)",
+        ["Bye for now. See you soon :)", "It was nice talking to you. See you soon :)",
          "One is glad to be of service! Have a great day ahead :)",
          "It was nice chatting with you. May the odds be ever in your favour! :)",
          "I enjoyed our chat! May the force be with you Obi! :)"]
     ],
     [
         r"(.*)",
-        ["I'm sorry, this is out of the scope of IT chatbot, please log a ticket and type 'quit' to exit the chat or "
-         "rephrase your question."]
+        ["I'm sorry, this is out of the scope of IT chatbot, please log a ticket with our customer support"
+         " and type 'quit' to exit the chat or rephrase your question."]
     ],
 ]
 
 # default message at the start of chat
-print("Welcome to our new automated IT support powered by NLTK an open source Natural Language Processing toolkit. ",
+print("\nWelcome to our new automated IT support powered by NLTK an open source Natural Language Processing toolkit. ",
       "\n\nPlease start by typing 'Hello, my name is...'\nType 'quit' to leave this chat.")
 
 # Create Chat Bot
