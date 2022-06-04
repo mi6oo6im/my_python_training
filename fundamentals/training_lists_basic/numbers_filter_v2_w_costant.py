d68c6538338cce469a8c3071052aa3465b570a09
Mihail Istiliyanov <54684159+mi6oo6im@users.noreply.github.com>
2022-06-04 09:29:52 +0100

    Update numbers_filter_v2_w_costant.py

--- fundamentals/training_lists_basic/numbers_filter_v2_w_costant.py
+++ fundamentals/training_lists_basic/numbers_filter_v2_w_costant.py
@@ -1,24 +1,22 @@
 numbers_qty = int(input())
 num_list = []
 filtered_list = []
-
+FILTER_EVEN = 'even'
+FILTER_ODD = 'odd'
+FILTER_POSITIVE = 'positive'
+FILTER_NEGATIVE = 'negative'
 
 for _ in range(numbers_qty):
     current_number = int(input())
     num_list.append(current_number)
 filter_text = input()
 
-FILTER_EVEN = filter_text == 'even'
-FILTER_ODD = filter_text == 'odd'
-FILTER_POSITIVE = filter_text == 'positive'
-FILTER_NEGATIVE = filter_text == 'negative'
-
 for number in num_list:
     filter_command = (
-        FILTER_EVEN and number % 2 == 0 or
-        FILTER_ODD and number % 2 != 0 or
-        FILTER_POSITIVE and number >= 0 or
-        FILTER_NEGATIVE and number < 0
+        filter_text == FILTER_EVEN and number % 2 == 0 or
+        filter_text == FILTER_ODD and number % 2 != 0 or
+        filter_text == FILTER_POSITIVE and number >= 0 or
+        filter_text == FILTER_NEGATIVE and number < 0
     )
     if filter_command:
         filtered_list.append(number)
