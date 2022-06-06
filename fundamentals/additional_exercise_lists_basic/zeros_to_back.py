2a3847f6d55d05b5d86ab8573480d7d6669cca9f
Mihail Istiliyanov <54684159+mi6oo6im@users.noreply.github.com>
2022-06-06 18:40:10 +0100

    Update zeros_to_back.py

--- fundamentals/additional_exercise_lists_basic/zeros_to_back.py
+++ fundamentals/additional_exercise_lists_basic/zeros_to_back.py
@@ -4,4 +4,5 @@ for i in range(len(numbers_list) - 1, -1, -1):
     if current_number == '0':
         numbers_list.pop(i)
         numbers_list.append(current_number)
+numbers_list = [int(x) for x in numbers_list]
 print(numbers_list)
