e00a00f09a57dc6837dec39df64959bb02f4e923
Mihail Istiliyanov <54684159+mi6oo6im@users.noreply.github.com>
2022-08-02 17:55:09 +0100

    basic web project

---
+++ training_projects/django_project_phonebook/basic_web_project/djangoproject/djangoproject/phonebook/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+
+# Create your models here.
+
+
+class Contact(models.Model):
+    name = models.CharField(max_length=30)
+    number = models.CharField(max_length=20)
