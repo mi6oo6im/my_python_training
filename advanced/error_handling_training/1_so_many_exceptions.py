16fc1b3bf770181d8ace3d6c13f711bf45ed5b27
Mihail Istiliyanov <54684159+mi6oo6im@users.noreply.github.com>
2023-02-06 14:07:53 +0000

    Create 1_so_many_exceptions.py

---
+++ advanced/error_handling_training/1_so_many_exceptions.py
@@ -0,0 +1,12 @@
+numbers_list = list(map(float, input().split(", ")))
+result = 1
+
+
+for i in range(len(numbers_list)):
+    number = numbers_list[i]
+    if number <= 5:
+        result *= number
+    elif 5 < number <= 10:
+        result /= number
+
+print(result)
