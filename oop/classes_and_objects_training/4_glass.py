66a79d35caf3142fb9b57fe722ac505875eb986b
Mihail Istiliyanov <54684159+mi6oo6im@users.noreply.github.com>
2023-02-25 10:34:52 +0000

    Create 4_glass.py

---
+++ oop/classes_and_objects_training/4_glass.py
@@ -0,0 +1,27 @@
+class Glass:
+    capacity = 250
+
+    def __init__(self):
+        self.content = 0
+
+    def fill(self, ml: int):
+        if self.capacity >= self.content + ml:
+            self.content += ml
+            return f"Glass filled with {ml} ml"
+        return f"Cannot add {ml} ml"
+
+    def empty(self):
+        self.content = 0
+        return "Glass is now empty"
+
+    def info(self):
+        space_left = self.capacity - self.content
+        return f"{space_left} ml left"
+
+
+glass = Glass()
+print(glass.fill(100))
+print(glass.fill(200))
+print(glass.empty())
+print(glass.fill(200))
+print(glass.info())
