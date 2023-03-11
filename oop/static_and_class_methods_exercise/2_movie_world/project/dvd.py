f3e82cf21853df4d416a3b328db9d3f9f22a7b06
Mihail Istiliyanov <54684159+mi6oo6im@users.noreply.github.com>
2023-03-11 21:35:42 +0000

    Create dvd.py

---
+++ oop/static_and_class_methods_exercise/2_movie_world/project/dvd.py
@@ -0,0 +1,32 @@
+class DVD:
+    MONTHS_Names = {
+        1: "January",
+        2: "February",
+        3: "March",
+        4: "April",
+        5: "May",
+        6: "June",
+        7: "July",
+        8: "August",
+        9: "September",
+        10: "October",
+        11: "November",
+        12: "December"
+    }
+
+    def __init__(self, name: str, id: int, creation_year: int, creation_month: str, age_restriction: int):
+        self.name = name
+        self.id = id
+        self.creation_year = creation_year
+        self.creation_month = creation_month
+        self.age_restriction = age_restriction
+        self.is_rented: bool = False
+
+    @classmethod
+    def from_date(cls, id: int, name: str, date: str, age_restriction: int):
+        _, month, year = date.split('.')
+        return cls(name, id, int(year), DVD.MONTHS_Names[int(month)], age_restriction)
+
+    def __repr__(self):
+        status = 'rented' if self.is_rented else 'not rented'
+        return f"{self.id}: {self.name} ({self.creation_month} {self.creation_year}) has age restriction {self.age_restriction}. Status: {status}"
