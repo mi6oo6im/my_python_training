663eb0b6319bc1387eee1468d78d0f0f1deda1c0
Mihail Istiliyanov <54684159+mi6oo6im@users.noreply.github.com>
2023-03-12 10:17:53 +0000

    Create document.py

---
+++ oop/static_and_class_methods_exercise/3_document_management/project/document.py
@@ -0,0 +1,34 @@
+from typing import List
+
+from project.category import Category
+from project.topic import Topic
+
+
+class Document:
+    def __init__(self, id: int, category_id: int, topic_id: int, file_name: str):
+        self.id = id
+        self.category_id = category_id
+        self.topic_id = topic_id
+        self.file_name = file_name
+        self.tags: List[str] = []
+
+    @classmethod
+    def from_instances(cls, id: int, category: Category, topic: Topic, file_name: str):
+        category_id = category.id
+        topic_id = topic.id
+        return cls(id, category_id, topic_id, file_name)
+
+    def add_tag(self, tag_content: str):
+        if tag_content not in self.tags:
+            self.tags.append(tag_content)
+
+    def remove_tag(self, tag_content: str):
+        if tag_content in self.tags:
+            self.tags.remove(tag_content)
+
+    def edit(self, file_name: str):
+        self.file_name = file_name
+
+    def __repr__(self):
+        return f"Document {self.id}: {self.file_name}; category" \
+               f" {self.category_id}, topic {self.topic_id}, tags: {', '.join(self.tags)}"
