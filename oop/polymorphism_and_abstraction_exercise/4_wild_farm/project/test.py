a91787f2c02fa7de7b077fe809421d9e8ac7a187
Mihail Istiliyanov <54684159+mi6oo6im@users.noreply.github.com>
2023-03-16 12:50:45 +0000

    Create test.py

---
+++ oop/polymorphism_and_abstraction_exercise/4_wild_farm/project/test.py
@@ -0,0 +1,13 @@
+from project.animals.birds import Owl, Hen
+from project.food import Meat, Vegetable, Fruit
+
+hen = Hen("Harry", 10, 10)
+veg = Vegetable(3)
+fruit = Fruit(5)
+meat = Meat(1)
+print(hen)
+print(hen.make_sound())
+hen.feed(veg)
+hen.feed(fruit)
+hen.feed(meat)
+print(hen)
