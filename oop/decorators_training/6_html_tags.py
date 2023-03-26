21f5e0ba1026b52e5a327276cd546c8d10e314a7
Mihail Istiliyanov <54684159+mi6oo6im@users.noreply.github.com>
2023-03-26 11:37:43 +0100

    Create 6_html_tags.py

---
+++ oop/decorators_training/6_html_tags.py
@@ -0,0 +1,15 @@
+def tags(tag):
+    def decorator(func):
+        def wrapper(*args):
+            return f"<{tag}>{func(*args)}</{tag}>"
+
+        return wrapper
+
+    return decorator
+
+
+@tags('h1')
+def to_upper(text):
+    return text.upper()
+print(to_upper('hello'))
+
