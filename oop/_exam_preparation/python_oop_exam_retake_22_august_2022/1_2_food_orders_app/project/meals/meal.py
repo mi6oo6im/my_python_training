d36ed5f3c8fd8a11e553f933f0d3567bb524bb1f
Mihail Istiliyanov <54684159+mi6oo6im@users.noreply.github.com>
2023-04-02 16:07:48 +0100

    Create meal.py

---
+++ oop/_exam_preparation/python_oop_exam_retake_22_august_2022/1_2_food_orders_app/project/meals/meal.py
@@ -0,0 +1,35 @@
+from abc import ABC, abstractmethod
+
+
+class Meal(ABC):
+    @abstractmethod
+    def __init__(self, name: str, price: float, quantity: int):
+        self.name = name
+        self.price = price
+        self.quantity = quantity
+
+    @property
+    def name(self):
+        return self.__name
+
+    @name.setter
+    def name(self, value):
+        if value.strip() == '':
+            raise ValueError("Name cannot be an empty string!")
+
+        self.__name = value
+
+    @property
+    def price(self):
+        return self.__price
+
+    @price.setter
+    def price(self, value):
+        if value <= 0.0:
+            raise ValueError("Invalid price!")
+
+        self.__price = value
+
+    @abstractmethod
+    def details(self):
+        ...
