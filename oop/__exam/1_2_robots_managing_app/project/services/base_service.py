ef98bb68409cabe62d719246c647655c6500a24e
Mihail Istiliyanov <54684159+mi6oo6im@users.noreply.github.com>
2023-04-08 17:21:43 +0100

    Create base_service.py

---
+++ oop/__exam/1_2_robots_managing_app/project/services/base_service.py
@@ -0,0 +1,46 @@
+from abc import ABC, abstractmethod
+from typing import List
+from project.robots.base_robot import BaseRobot
+
+
+class BaseService(ABC):
+
+    @abstractmethod
+    def __init__(self, name: str, capacity: int):
+        self.name = name
+        self.capacity = capacity
+        self.robots: List[BaseRobot] = []
+
+    @property
+    def name(self):
+        return self.__name
+
+    @name.setter
+    def name(self, value):
+        if value.strip() == '':
+            raise ValueError("Service name cannot be empty!")
+
+        self.__name = value
+
+    @property
+    def capacity(self):
+        return self.__capacity
+
+    @capacity.setter
+    def capacity(self, value):
+        if value <= 0:
+            raise ValueError("Service capacity cannot be less than or equal to 0!")
+
+        self.__capacity = value
+
+    @property
+    @abstractmethod
+    def service_type(self):
+        ...
+
+    def details(self):
+        robot_names = ' '.join([r.name for r in self.robots]) if self.robots else 'none'
+        result = f"{self.name} {self.service_type}:\n"\
+                 f"Robots: {robot_names}"
+
+        return result
