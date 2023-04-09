de846f29915f10076134bdaa65e8662adf2df99b
Mihail Istiliyanov <54684159+mi6oo6im@users.noreply.github.com>
2023-04-09 22:49:31 +0100

    Delete recursive_factorial.py

--- algorithms_with_python/training_recursions_and_backtracking/recursive_factorial.py
+++
@@ -1,8 +0,0 @@
-def factorial_recursion(number: int):
-    if number == 0:
-        return 1
-    return number * factorial_recursion(number - 1)
-
-
-factorial_num = int(input())
-print(factorial_recursion(factorial_num))
