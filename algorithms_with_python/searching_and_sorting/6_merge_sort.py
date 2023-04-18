8d5d5039b39bc01fa44b15768f98632d58bb27a4
Mihail Istiliyanov <54684159+mi6oo6im@users.noreply.github.com>
2023-04-18 18:36:06 +0100

    Create 6_merge_sort.py

---
+++ algorithms_with_python/searching_and_sorting/6_merge_sort.py
@@ -0,0 +1,43 @@
+def merge_arrays(left, right):
+    result = [None] * (len(left) + len(right))
+    left_idx = 0
+    right_idx = 0
+    res_idx = 0
+
+    while left_idx < len(left) and right_idx < len(right):
+        if left[left_idx] <= right[right_idx]:
+            result[res_idx] = left[left_idx]
+            left_idx += 1
+        else:
+            result[res_idx] = right[right_idx]
+            right_idx += 1
+
+        res_idx += 1
+
+    while left_idx < len(left):
+        result[res_idx] = left[left_idx]
+        left_idx += 1
+        res_idx += 1
+
+    while right_idx < len(right):
+        result[res_idx] = right[right_idx]
+        right_idx += 1
+        res_idx += 1
+
+    return result
+
+
+def merge_sort(nums):
+    if len(nums) == 1:
+        return nums
+
+    mid_idx = len(nums) // 2
+    left = nums[:mid_idx]
+    right = nums[mid_idx:]
+
+    return merge_arrays(merge_sort(left), merge_sort(right))
+
+
+nums = [int(x) for x in input().split()]
+result = merge_sort(nums)
+print(*result, sep=' ')
