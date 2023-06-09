2805eaa30002878b0f0d4c8bb531690d26027e6c
Mihail Istiliyanov <54684159+mi6oo6im@users.noreply.github.com>
2023-06-09 14:42:04 +0100

    Update settings.py

--- web/tasks_basic_project/djangoTasks/djangoTasks/settings.py
+++ web/tasks_basic_project/djangoTasks/djangoTasks/settings.py
@@ -90,7 +90,7 @@ DATABASES = {
         "ENGINE": "django.db.backends.postgresql",
         "NAME": "tasks",
         "USER": "mi6oo6im",
-        "PASSWORD": "Parola1@",
+        "PASSWORD": "*******",
         "HOST": "127.0.0.1",
         "PORT": "5432",
     }
